c5af241b58b6f3a04e92eb78257e45915bb2909d
Marcelo Silva <marcelo_810@hotmail.com>
2023-11-23 14:10:02 -0300

    feat/carousel-scroll

--- src/components/home/Banner/VideoContainer.tsx
+++ src/components/home/Banner/VideoContainer.tsx
@@ -12,7 +12,7 @@ export default function VideoDefault({ videoImage, img }: VideoDefaultProps) {
         videoImage === 'banner'
       }? 'w-[646px] h-[334px]': w-[432px] h-[260px]`}
     >
-      <Image src={img} width={646} height={334} alt="" />
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/6Gi4_GJXO4I?si=RJ1ESCLaacu7nDKG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   );
 }
--- src/components/home/Carousel/FrontEnd.tsx
+++ src/components/home/Carousel/FrontEnd.tsx
@@ -1,23 +1,41 @@
+import { useState } from 'react';
 import { frontEnd } from '@/@helpers/videos';
 import { Banner } from '../Banner';
 import { useSnapCarousel } from 'react-snap-carousel';
+import { CaretLeft, CaretRight } from "@phosphor-icons/react/dist/ssr";
 
 export default function FrontEnd() {
-  const { scrollRef } = useSnapCarousel();
+  const { scrollRef, next, prev } = useSnapCarousel();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    next();
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handlePrev = () => {
+    prev();
+    setCurrentIndex((prevIndex) => prevIndex - 1);
+  };
+
   return (
     <>
-      <h2></h2>
+      <h2 className="bg-yellow-500 text-[35px] w-[204px] text-center "></h2>
       <div className="flex gap-5">
+        <button onClick={handlePrev} disabled={currentIndex === 0}>
+          <CaretLeft size={40} />
+        </button>
         <ul
           className="gap-4 my-3 mb-10"
           ref={scrollRef}
           style={{
             display: 'flex',
-            overflow: 'auto',
+            overflowX: 'hidden',
+            overflowY: 'hidden',
             scrollSnapType: 'x mandatory',
           }}
         >
-          {frontEnd.map(frontEnd => (
+          {frontEnd.map((frontEnd) => (
             <li
               key={frontEnd.id}
               className="bg-gray-300 text-5xl w-(250px) h-(250px) flex-shrink-0 text-white flex justify-center items-center overflow-hidden"
@@ -26,6 +44,9 @@ export default function FrontEnd() {
             </li>
           ))}
         </ul>
+        <button onClick={handleNext} disabled={currentIndex === frontEnd.length - 1}>
+          <CaretRight size={40} />
+        </button>
       </div>
     </>
   );
